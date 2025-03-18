18cfbd127ce435a199efaec62764c30f0c7af581
Amey Darokar <129416212+Amey-sd@users.noreply.github.com>
2025-03-18 14:37:31 +0530

    conan

--- src/shader.cpp
+++ src/shader.cpp
@@ -1,4 +1,4 @@
-#include <shader.h>
+#include "shader.h"
 
 #include <fstream>
 #include <iostream>
@@ -7,13 +7,18 @@
 #include <vector>
 
 #include <GL/glew.h>
+#include <omp.h>
 
 static std::string readFile(const std::string &file) {
   std::string VertexShaderCode;
   std::ifstream ifs(file, std::ios::in);
   if (ifs.is_open()) {
     std::stringstream ss;
-    ss << ifs.rdbuf();
+    #pragma omp parallel
+    {
+      #pragma omp single
+      ss << ifs.rdbuf();
+    }
     return ss.str();
   } else {
     throw "Failed to open file: " + file;
@@ -33,14 +38,17 @@ static GLuint compileShader(const std::string &shaderSource,
   GLint success = GL_FALSE;
   glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
   if (success == GL_FALSE) {
-
     GLint maxLength = 0;
     glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
 
     // The maxLength includes the NULL character
     std::vector<GLchar> infoLog(maxLength);
     glGetShaderInfoLog(shader, maxLength, &maxLength, &infoLog[0]);
-    std::cout << infoLog[0] << std::endl;
+    #pragma omp parallel
+    {
+      #pragma omp single
+      std::cout << infoLog[0] << std::endl;
+    }
     glDeleteShader(shader);
     throw "Failed to compile the shader.";
   }
@@ -51,14 +59,24 @@ static GLuint compileShader(const std::string &shaderSource,
 GLuint createShaderProgram(const std::string &vertexShaderFile,
                            const std::string &fragmentShaderFile) {
 
-  // Compile vertex and fragment shaders.
-  std::cout << "Compiling vertex shader: " << vertexShaderFile << std::endl;
-  GLuint vertexShader =
-      compileShader(readFile(vertexShaderFile), GL_VERTEX_SHADER);
+  GLuint vertexShader, fragmentShader;
 
-  std::cout << "Compiling fragment shader: " << fragmentShaderFile << std::endl;
-  GLuint fragmentShader =
-      compileShader(readFile(fragmentShaderFile), GL_FRAGMENT_SHADER);
+  #pragma omp parallel sections
+  {
+    #pragma omp section
+    {
+      // Compile vertex shader
+      std::cout << "Compiling vertex shader: " << vertexShaderFile << std::endl;
+      vertexShader = compileShader(readFile(vertexShaderFile), GL_VERTEX_SHADER);
+    }
+
+    #pragma omp section
+    {
+      // Compile fragment shader
+      std::cout << "Compiling fragment shader: " << fragmentShaderFile << std::endl;
+      fragmentShader = compileShader(readFile(fragmentShaderFile), GL_FRAGMENT_SHADER);
+    }
+  }
 
   // Create shader program.
   GLuint program = glCreateProgram();
@@ -75,17 +93,33 @@ GLuint createShaderProgram(const std::string &vertexShaderFile,
     if (maxLength > 0) {
       std::vector<GLchar> infoLog(maxLength);
       glGetProgramInfoLog(program, maxLength, NULL, &infoLog[0]);
-      std::cout << infoLog[0] << std::endl;
+      #pragma omp parallel
+      {
+        #pragma omp single
+        std::cout << infoLog[0] << std::endl;
+      }
       throw "Failed to link the shader.";
     }
   }
 
   // Detach shaders after a successful link.
-  glDetachShader(program, vertexShader);
-  glDetachShader(program, fragmentShader);
+  #pragma omp parallel sections
+  {
+    #pragma omp section
+    glDetachShader(program, vertexShader);
 
-  glDeleteShader(vertexShader);
-  glDeleteShader(fragmentShader);
+    #pragma omp section
+    glDetachShader(program, fragmentShader);
+  }
+
+  #pragma omp parallel sections
+  {
+    #pragma omp section
+    glDeleteShader(vertexShader);
+
+    #pragma omp section
+    glDeleteShader(fragmentShader);
+  }
 
   return program;
 }
